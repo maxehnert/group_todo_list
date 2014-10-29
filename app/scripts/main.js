525cc671d7a1258d5b7322865c5a3e3a2989199e
Mike Zevitas <mzevitas@gmail.com>
2014-10-29 16:22:09 -0400

    function stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,4 @@
-var my_server = 'http://tiy-atl-fe-server.herokuapp.com/collections/mandmlist2';
+var my_server = 'http://tiy-atl-fe-server.herokuapp.com/collections/mandmlist3';
 
 
 var ToDo= function (options){
@@ -13,27 +13,24 @@ var ToDo= function (options){
 
 };
 //collections of ToDo
-var todo_list = [];
+var todo_list;
 
 
 var task_template = $('#task_items').html();
 var rendered = _.template(task_template);
 
 $.getJSON(my_server).done( function(data){
+
 todo_list = data;
     _.each(todo_list, function (item){
-
-       $('#todoList', 'todo_list').prepend(rendered(item));
+       $('#todoList').prepend(rendered(item));
     });
 });
 
 var task, contents;
 
 
-// var icon = $('<a href="#"><img src="http://f.cl.ly/items/3F2D2J201Q2F3I072632/cross5.png"></a>').load(function() {
-//   $(this).width(16).height(16).appendTo('li');
-//
-// });
+
 
 
 
@@ -44,14 +41,14 @@ var self = this;
     contents = $('#text').val() + '<button class="remove">x</button>';
 
 
-//     var text = $('#text').val() + '<button>x</button>';
-//     if(text.length){
-//         $('<li />', {html: text}).appendTo('#todoList').append(task.elem)
-//     }
-//
-//
+
 $('ul').on('click','button' , function(el){
     $(this).parent().remove();
+
+    $.ajax ({
+      type: 'DELETE',
+      url: my_server + "/" + todo_modifier._id
+    });
 
 var q = $('#todoList li').length - 0;
 $('#counter').html(q);
@@ -116,7 +113,10 @@ var w = $('#todoList .done').length;
 
     $('delete').removeClass();
 
+
+
 }
+
 $.ajax ({
 type: 'PUT',
 url: my_server + "/" + todo_modifier._id,
@@ -124,9 +124,6 @@ data: todo_modifier
 });
 
 
-// $.ajax ({
-//   type: 'DELETE',
-//   url: my_server + "/" + todo_modifier._id
-// });
+
 
 });
